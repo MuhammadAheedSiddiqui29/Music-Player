96a97e2e0c3566132b2c40727192453562f5bfcf
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-02-07 21:01:04 +0500

    corrected the onloadedmetadata mistake

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let progress = document.querySelector("#progress");
 let song = document.querySelector("#song");
 let ctrlIcon = document.querySelector("#ctrlIcon");
 
-song.onloadmetadata = function(){
+song.onloadedmetadata = function(){
     progress.max = song.duration;
     progress.value = song.currentTime;
 }
@@ -33,28 +33,3 @@ progress.onchange = function(){
     ctrlIcon.classList.remove("fa-play");
 }
 
-
-
-//gpt
-
-// function playPause(){
-//     if(song.paused){
-//         song.play();
-
-//         ctrlIcon.classList.add("fa-pause");
-//         ctrlIcon.classList.remove("fa-play");
-//     }
-//     else{
-//         song.pause();
-//         ctrlIcon.classList.remove("fa-pause");
-//         ctrlIcon.classList.add("fa-play");
-//     }
-// }
-
-// song.ontimeupdate = function(){
-//     progress.value = song.currentTime;
-// };
-
-// progress.oninput = function(){
-//     song.currentTime = progress.value;
-// }
